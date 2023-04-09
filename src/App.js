f1d25879d31d02e72e9d438c5cc54d31e02d5fe8
RenanTCastro <renancastrotut@gmail.com>
2023-04-09 00:59:33 -0300

    Criado tela minha conta

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 // import { EditarLiga } from "./pages/EditarLiga/EditarLiga";
 // import { VisualizarLiga } from "./pages/VisualizarLiga/VisualizarLiga";
 // import { Login } from "./pages/Login/Login";
-import { Cadastro } from "./pages/Cadastro/Cadastro";
+// import { Cadastro } from "./pages/Cadastro/Cadastro";
+import { MinhaConta } from "./pages/MinhaConta/MinhaConta";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Cadastro />
+        <MinhaConta />
       </header>
     </div>
   );
---
+++ src/pages/MinhaConta/MinhaConta.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import Logo from '../../images/logo.svg';
+ 
+import "./MinhaConta.css";
+
+export function MinhaConta(){
+
+    return(
+        <div>
+            <img src={Logo} alt="Logo" style={{marginTop: "80px"}}/>
+            <h1 className='minhaConta--title'>Olá, pronto para mitar?</h1>
+            <p className='minhaConta--description'>
+                Bem-vindo(a) à sua conta! Gostaríamos de lembrar que para cada liga criada, um crédito 
+                será consumido da sua conta. Por favor, tenha em mente que esses créditos não serão 
+                devolvidos caso você decida excluir uma liga, então é importante usar com cuidado. 
+            </p>
+
+            <p className='minhaConta--credit-text'>Seus creditos: 1</p>
+            <button 
+                className='minhaConta--buttons' 
+                style={{backgroundColor: ' #FE7301'}}>
+                    Comprar créditos
+            </button>
+
+            <p className='minhaConta--description'>Seu código de usuário é: AB233. </p>
+
+            <div className='minhaconta--table-ligas-container-header'>
+                <p className='minhaConta--table-title'>Suas ligas</p>
+                <button 
+                className='minhaConta--buttons' 
+                style={{backgroundColor: '#21A70B'}}>
+                    Criar liga
+                </button>
+            </div>
+            <div className='minhaconta--table-ligas-container-body'>
+                <div>Liga MilGrau</div>
+                <div>Rodada premiada</div>
+            </div>
+
+            <div style={{marginBottom: "40px"}}>
+                <button 
+                    className='minhaConta--buttons' 
+                    style={{backgroundColor: 'white', color:'#222222', padding: "0 50px"}}>
+                        Sair
+                </button>
+                <button 
+                    className='minhaConta--buttons' 
+                    style={{backgroundColor: '#FE0101'}}>
+                        Excluir conta
+                </button>
+            </div>
+        </div>
+    );
+}
