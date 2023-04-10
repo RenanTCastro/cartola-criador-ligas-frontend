9d38d7de2bbfea52b960427a6c5a8c364ad213f2
RenanTCastro <renancastrotut@gmail.com>
2023-04-10 20:23:36 -0300

    Responsividade página Minha conta

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 
-// import { EditarLiga } from "./pages/EditarLiga/EditarLiga";
+//import { EditarLiga } from "./pages/EditarLiga/EditarLiga";
 // import { VisualizarLiga } from "./pages/VisualizarLiga/VisualizarLiga";
 // import { Login } from "./pages/Login/Login";
-// import { Cadastro } from "./pages/Cadastro/Cadastro";
-import { MinhaConta } from "./pages/MinhaConta/MinhaConta";
+import { Cadastro } from "./pages/Cadastro/Cadastro";
+//import { MinhaConta } from "./pages/MinhaConta/MinhaConta";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <MinhaConta />
+        <Cadastro />
       </header>
     </div>
   );
